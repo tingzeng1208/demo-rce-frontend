06cd74bd87baa2b95af3d0c58c7147f9c3866ea9
tingzeng-at-594818642338 <ting_zeng@yhoo.com>
2022-01-11 23:27:25 -0500

    clean up

--- src/story/EditBox.tsx
+++ src/story/EditBox.tsx
@@ -19,8 +19,6 @@ const textInputstyle : CSS.Properties  = {
   height: '20px'
 };
 
-const testContent = 'content';
-
 const leftGridSize = 8;
 
 const rightGridSize = 4;
@@ -64,33 +62,8 @@ export const EditForm: React.FunctionComponent<FormProps> = ({id: number,
     <Grid row>
       <Grid col={12}><Button type='submit'>Submit</Button></Grid>
     </Grid>
-    {/* <Grid row>
-      <Grid col={leftGridSize}>Address:</Grid>
-      <Grid col={rightGridSize}>{Address}</Grid>
-    </Grid>
-    <Grid row>
-      <Grid col={leftGridSize}>Email:</Grid>
-      <Grid col={rightGridSize}>{email}</Grid>
-    </Grid>
-    <Grid row>
-      <Grid col={leftGridSize}>State:</Grid>
-      <Grid col={rightGridSize}>{State}</Grid>
-    </Grid>
-    <Grid row>
-      <Grid col={leftGridSize}>Zip:</Grid>
-      <Grid col={rightGridSize}>{ZIP}</Grid>
-    </Grid> */}
+    
   </GridContainer>
   </form>
   </div>;
-        // <div>
-        //   <div>Details:</div>
-        //   <div>Id: &nbsp;{id}</div>
-        //   <div>Applicant Name: &nbsp;{ApplicantName}</div>
-        //   <div>Application Status: &nbsp;{status? 'Active' : 'Inactive'}</div>
-        //   <div>Address: &nbsp;{Address}</div>
-        //   <div>Email: &nbsp;{email}</div>
-        //   <div>State: &nbsp;{State}</div>
-        //   <div>Zip: &nbsp;{ZIP}</div>
-        // </div>
 }
