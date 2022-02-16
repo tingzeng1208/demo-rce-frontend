7326c3d5a6070b4bde7e9a3ef13f49f4d41a9cbc
tingzeng-at-594818642338 <ting_zeng@yhoo.com>
2022-02-15 22:58:21 -0500

    more for icon

--- src/components/ReactIcon/Icon.stories.tsx
+++ src/components/ReactIcon/Icon.stories.tsx
@@ -3,6 +3,11 @@ import {ReactMakeIcon} from './Icon';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { FaRocket } from 'react-icons/fa';
 import {ImNewspaper} from 'react-icons/im';
+import { makeUSWDSIcon, IconProps } from '../Icon/Icon';
+import AccessibilityNewSvg from '../../../node_modules/uswds/dist/img/usa-icons/accessible_forward.svg';
+
+
+
 
 export default {
   title: 'Example/Icons/allIcon',
@@ -12,6 +17,8 @@ export default {
 } as ComponentMeta<typeof ReactMakeIcon>;
 
 const Template: ComponentStory<typeof ReactMakeIcon> = (args) => <ReactMakeIcon {...args} />;
+const IconAccessibilityNew: React.ComponentType<IconProps> =
+  makeUSWDSIcon(AccessibilityNewSvg)
 
 export const ReactMakeIcon1 = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
@@ -19,6 +26,14 @@ ReactMakeIcon1.args = {
   Component: 'FaRocket',
   className: 'App-logo',
 };
+
+const NewSvg = ()=>{
+  return (
+<svg class="usa-icon" aria-hidden="true" focusable="false" role="img">
+        <use xlink:href="/assets/img/sprite.svg#accessibility_new"></use>
+      </svg>
+  )
+}
 
 export const allIcons = (): React.ReactElement =>{
   return <div>
